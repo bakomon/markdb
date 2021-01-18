87dd18a73d155b6053c2876a11218da18028ef0c
Bakomon <43160154+bakomon@users.noreply.github.com>
2021-01-18 17:38:19 +0800

    Update comic-reader.js

--- reader/comic-reader.js
+++ reader/comic-reader.js
@@ -603,16 +603,19 @@
       var imgMS = dataMS('images');
       createImage(imgMS);
     } else if (wh.indexOf('mangaku') != -1) { //script
-      if (el('#data_mgk')) {
-        var par_elm = el('.singlep .section_ad.group_ad').parentNode.nextElementSibling; //dtxx
-        var r_area = document.createElement('div');
-        r_area.id = 'readerarea';
-        r_area.innerHTML = el('#data_mgk').innerHTML;
-        par_elm.parentNode.insertBefore(r_area, par_elm);
-        par_elm.parentNode.removeChild(par_elm);
-        
-        startImage();
-      }
+      var mgk_chk = setInterval(function() {
+        if (el('#data_mgk')) {
+          clearInterval(mgk_chk);
+          var par_elm = el('.singlep .section_ad.group_ad').parentNode.nextElementSibling; //dtxx
+          var r_area = document.createElement('div');
+          r_area.id = 'readerarea';
+          r_area.innerHTML = el('#data_mgk').innerHTML;
+          par_elm.parentNode.insertBefore(r_area, par_elm);
+          par_elm.parentNode.removeChild(par_elm);
+          
+          startImage();
+        }
+      }, 100);
       
       /* new
       var el_mgk = el('#contentwrap');
