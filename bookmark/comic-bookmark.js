cba8b9e5c7cc3fe29749824ee0311d8f80a535ae
Bakomon <43160154+bakomon@users.noreply.github.com>
2021-01-19 15:17:29 +0800

    Update comic-bookmark.js

--- bookmark/comic-bookmark.js
+++ bookmark/comic-bookmark.js
@@ -180,7 +180,7 @@
     for (var key in json) {
       arr.push(json[key]);
       // check if comic data exist and show bookmark
-      var title_rgx = el('title').innerHTML.replace(/\s(bahasa\s)?indonesia/i, '').match(/^(?:manga|[kc]omi[kc])?\s?([^\-|\||–]+)(?:\s[\-|\||–])?/)[1];
+      var title_rgx = el('title').innerHTML.replace(/\s(bahasa\s)?indonesia/i, '').match(/^(?:manga|[kc]omi[kc])?\s?([^\-|\||–]+)(?:\s[\-|\||–])?/)[1].replace(/\s$/, '');
       title_rgx = new RegExp(title_rgx, 'ig');
       if (!query && (wp.indexOf(json[key].id) != -1 || json[key].id.replace(/\-/g, ' ').search(title_rgx) != -1 || json[key].title.search(title_rgx) != -1 || json[key].alternative.search(title_rgx) != -1)) {
         cm_ID = json[key].id;
