5d14b30434814bb5bf0c2bb2f27abcbb9a7d81ba
Bakomon <43160154+bakomon@users.noreply.github.com>
2021-01-20 23:46:31 +0800

    Update comic-bookmark.js

--- bookmark/comic-bookmark.js
+++ bookmark/comic-bookmark.js
@@ -181,9 +181,9 @@
     for (var key in json) {
       arr.push(json[key]);
       // check if comic data exist and show bookmark
-      var title_rgx = el('title').innerHTML.replace(/\s(bahasa\s)?indonesia/i, '').match(/^(?:manga|[kc]omi[kc])?\s?([^\-|\||–]+)(?:\s[\-|\||–])?/)[1].replace(/\s$/, '');
+      var title_rgx = el('title').innerHTML.replace(/\s(bahasa\s)?indonesia/i, '').replace(/(man(ga|hwa|hua)|[kc]omi[kc])\s/i, '').match(/^([^\-|\||–]+)(?:\s[\-|\||–])?/)[1].replace(/\s$/, '');
       title_rgx = new RegExp(title_rgx, 'ig');
-      if (!query && (wp.indexOf(json[key].id) != -1 || json[key].id.replace(/\-/g, ' ').search(title_rgx) != -1 || json[key].title.search(title_rgx) != -1 || json[key].alternative.search(title_rgx) != -1)) {
+      if (!query && wp != '/' && (wp.indexOf(json[key].id) != -1 || json[key].id.replace(/\-/g, ' ').search(title_rgx) != -1 || json[key].title.search(title_rgx) != -1 || json[key].alternative.search(title_rgx) != -1)) {
         cm_ID = json[key].id;
         is_comic = true;
         bc_showData();
