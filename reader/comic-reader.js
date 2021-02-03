c2746d97afb0ce6b73091e5a9205e30535472f8f
Bakomon <43160154+bakomon@users.noreply.github.com>
2021-02-03 11:49:22 +0800

    Update comic-reader.js

--- reader/comic-reader.js
+++ reader/comic-reader.js
@@ -980,6 +980,7 @@
   }, 100);
   
   if ((wp.search(wk1) != -1 || wl.search.search(wk1) != -1 || el('title').innerHTML.search(/\sch\.?(apter)?\s/i) != -1) && wl.search.indexOf('project') == -1 && wh.indexOf('tenseiscans') == -1) {
+    console.log('chapter page');
     checkAll();
   }
 })();
