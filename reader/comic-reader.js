5d36215d264b36c04af0b61e0bb25002cf72a5b3
Bakomon <43160154+bakomon@users.noreply.github.com>
2021-02-09 17:34:13 +0800

    Update comic-reader.js

--- reader/comic-reader.js
+++ reader/comic-reader.js
@@ -154,7 +154,7 @@
   }
   
   function createBtn(img) {
-  	var readSize = localStorage.getItem(titleId) && !isMobile ? localStorage.getItem(titleId) : imgArea.offsetWidth;
+    var readSize = localStorage.getItem(zoomID) && !isMobile ? localStorage.getItem(zoomID) : imgArea.offsetWidth;
     var r_txt = '';
     // css control & main already in css tools
     // css reader
@@ -262,7 +262,7 @@
         }
         imgArea.style.cssText = 'max-width:'+ load_zm +'px !important;';
         el('.rc_zoom input').value = load_zm;
-        localStorage.setItem(titleId, load_zm);
+        localStorage.setItem(zoomID, load_zm);
       });
     });
     
@@ -829,8 +829,10 @@
   var wl = window.location;
   var wh = wl.hostname;
   var wp = wl.pathname;
-  var wk1 = /(\/|\-|\_|\d+)((ch|\/c)(ap(ter)?)?|ep(isode)?)(\/|\-|\_|\d+)/;
-  //var wk1 = /chapter(\/|\-)|\-bahasa|\-indonesia|ch\-|(\-|\/)\d+|(\-|\/)ep\d+|(chap|episode)\_|\/c\d+/;
+  var chapter_t_rgx = /\s(ch\.?(ap(ter)?)?|ep\.?(isode)?)(\s?\d+|\s)/i; //chek for <title>
+  var chapter_w_rgx = /(\/|\-|\_|\d+)((ch|\/c)(ap(ter)?)?|ep(isode)?)(\/|\-|\_|\d+)/i; //check for window.location
+  //var chapter_w_rgx = /chapter(\/|\-)|\-bahasa|\-indonesia|ch\-|(\-|\/)\d+|(\-|\/)ep\d+|(chap|episode)\_|\/c\d+/;
+  var id_w_rgx = /\/(?:(?:baca-)?(?:komik|manga|read|[a-z]{2}\/[^\/]+|(?:title|series|comics?)(?:\/\d+)?|(?:\d{4}\/\d{2})|p)[\/\-])?([^\/\n]+)\/?(?:list)?/i; //id from window.location
   var chcdn = false; //if image has wp.com or statically.io
   var chgi = false; //if google images
   var loadCDN = false;
@@ -839,7 +841,7 @@
   var isPause = false; //pause images from loading
   var isMobile = document.documentElement.classList.contains('is-mobile') ? true : false; //from comic tools
   var imgSize = ''; //image size
-  var checkPoint, imgArea, imgList, cdnName, titleId;
+  var checkPoint, imgArea, imgList, cdnName, titleId, wpId, zoomID;
   
   document.body.classList.add(wh.replace(/(w{3}|m)\./, ''));
   removeAADB(); //remove anti adblock notify mangacanblog
@@ -966,9 +968,11 @@
     }
   }, 100);
   
-  if ((wp.search(wk1) != -1 || wl.search.search(wk1) != -1 || el('title').innerHTML.search(/\sch\.?(apter)?\s/i) != -1) && wl.search.indexOf('project') == -1 && wh.search(/tenseiscans|komikempus/) == -1) {
-    titleId = el('title').innerHTML.replace(/&#{0,1}[a-z0-9]+;/ig, '').replace(/\([^\)]+\)/g, '').replace(/\s+/g, ' ').replace(/\s(bahasa\s)?indonesia/i, '').replace(/(man(ga|hwa|hua)|[kc]omi[kc])\s/i, '').match(/^([^\-|\||–]+)(?:\s[\-|\||–])?/)[1].replace(/\s$/, '').replace(/[^\s\w]/g, '').replace(/\s/g, '-').toLowerCase();
-    console.log('chapter page');
+  if ((wp.search(chapter_w_rgx) != -1 || wl.search.search(chapter_w_rgx) != -1 || el('title').innerHTML.search(chapter_t_rgx) != -1) && wl.search.indexOf('project') == -1 && wh.search(/tenseiscans|komikempus/) == -1) {
+    titleId = el('title').innerHTML.replace(/&#{0,1}[a-z0-9]+;/ig, '').replace(/\([^\)]+\)/g, '').replace(chapter_t_rgx, '').replace(/\s+/g, ' ').replace(/\s(bahasa\s)?indonesia/i, '').replace(/(man(ga|hwa|hua)|[kc]omi[kc])\s/i, '').match(/^([^\-|\||–]+)(?:\s[\-|\||–])?/)[1].replace(/\s$/, '').replace(/[^\s\w]/g, '').replace(/\s/g, '-').toLowerCase();
+    wpId = wp.match(id_w_rgx)[1].replace(/-bahasa-indonesia(-online-terbaru)?/i, '').replace(/\.html/i, '').toLowerCase();
+    zoomID = wh.search(/webtoons/i) != -1 ? wpId : titleId;
+    console.log('page: chapter');
     checkAll();
   }
 })();
