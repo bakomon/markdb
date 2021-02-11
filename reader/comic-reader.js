960456148869cbc942054ef3aa5a8f4baca8ad56
Bakomon <43160154+bakomon@users.noreply.github.com>
2021-02-11 13:36:26 +0800

    Update comic-reader.js

--- reader/comic-reader.js
+++ reader/comic-reader.js
@@ -335,7 +335,7 @@
     };
     
     // after html and js _reader loaded then auto click to stop page
-    if (el('.rc_reload').classList.contains('rc_hidden') && wh.indexOf('webtoons') == -1) el('.rc_stop').click();
+    if (el('.rc_reload').classList.contains('rc_hidden') && wh.search(/webtoons|mangadex/) == -1) el('.rc_stop').click();
   }
   
   function startImage(prnt, imgs) {
