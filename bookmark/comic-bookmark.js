2c2caf3e403653b20cb875d6377624d9ae6d2a91
Bakomon <43160154+bakomon@users.noreply.github.com>
2021-06-24 20:50:42 +0800

    Update comic-bookmark.js

--- bookmark/comic-bookmark.js
+++ bookmark/comic-bookmark.js
@@ -461,7 +461,7 @@
     b_txt += '<input class="bc_host _bc bc_100" type="text" placeholder="hostname">';
     b_txt += '<div class="flex bc_100"><input class="bc_url _bc bc_100" type="text" placeholder="URL"><button class="bc_url_open _bc flex f_middle bc_selected bc_hidden"><span>&#128279;</span> Open</button></div>';
     b_txt += '<input class="bc_read _bc bc_100" type="text" placeholder="Link to read (if web to read is different)">';
-    b_txt += '<div class="flex bc_100"><input class="bc_image _bc bc_100" type="text" placeholder="Cover image"><button class="bc_img_open _bc flex f_middle bc_selected bc_hidden"><span>&#128444;</span> Open</button>></div>';
+    b_txt += '<div class="flex bc_100"><input class="bc_image _bc bc_100" type="text" placeholder="Cover image"><button class="bc_img_open _bc flex f_middle bc_selected bc_hidden"><span>&#128444;</span> Open</button></div>';
     b_txt += '<div class="flex bc_100"><input class="bc_last _bc f_grow" type="date" title="Last Update"><button class="bc_date_before _bc bc_selected bc_hidden" onclick="document.querySelector(\'.bc_last\').valueAsDate = new Date(Number(this.dataset.date))">Before</button></div>';
     b_txt += '<input class="bc_similar _bc bc_100" type="text" placeholder="Similar comic ID">';
     b_txt += '</div>';// .bc_form
