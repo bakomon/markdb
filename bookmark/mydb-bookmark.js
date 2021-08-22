1fccef6f6ef55d04e7242dd3402b4f05ef298446
Bakomon <43160154+bakomon@users.noreply.github.com>
2021-08-23 04:00:59 +0800

    Update mydb-bookmark.js

--- bookmark/mydb-bookmark.js
+++ bookmark/mydb-bookmark.js
@@ -107,7 +107,7 @@
         g_val = el('.db_form .db_'+ g_arr[i]).value;
       }
       
-      if (g_arr[i].search(/title|alternative/) != -1) g_val = g_val.replace(/(“|”|")/g, '\\$1');
+      if (g_arr[i].search(/title|alternative/) != -1) g_val = g_val.replace(/"/g, '\\$1');
       if (g_arr[i] == 'host') g_val = getHostname(g_val);
       if (g_arr[i].search(/domain|project|url|read/) != -1) g_val = g_val.replace(/^(https?:)?\/\//g, '//').replace(/\/\/((w{3}|m(obile)?)\.)?/g, '//');
       if (mydb_select == 'source' && g_arr[i] == 'domain') g_val = g_val.replace(/\//g, '');
