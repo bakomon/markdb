59ce9cdec5902abf98d8a43c4b9209d21efda795
Bakomon <43160154+bakomon@users.noreply.github.com>
2021-10-22 16:33:41 +0800

    Update comic-reader.js

--- reader/comic-reader.js
+++ reader/comic-reader.js
@@ -61,8 +61,8 @@ function mydb_comic_reader() {
     removeElem('#disqus_thread');
     
     var disqus_load = document.createElement('div');
-    disqus_load.cssText = 'width:100%;text-align:center;';
-    disqus_load.innerHTML = '<button id="disqus_trigger" style="border:0;padding:5px 10px;font-size:20px;cursor:pointer;">Post a Comment</button>';
+    disqus_load.cssText = 'width:100%;';
+    disqus_load.innerHTML = '<div style="text-align:center;"><button id="disqus_trigger" style="border:0;padding:5px 10px;font-size:20px;cursor:pointer;">Post a Comment</button></div>';
     par_dsqs.appendChild(disqus_load);
     
     var disqus_new = document.createElement('div');
