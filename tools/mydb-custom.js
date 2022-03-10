09ccb040ede10a5bdb39ed57d76b36b99527f3b5
Bakomon <43160154+bakomon@users.noreply.github.com>
2022-03-11 06:53:12 +0800

    Update mydb-custom.js

--- tools/mydb-custom.js
+++ tools/mydb-custom.js
@@ -4,6 +4,34 @@ function mydb_custom() {
   mydb_x_loaded = true;
   if (el('#_loader')) el('#_loader').parentElement.removeChild(el('#_loader'));
   
+    // Cookies https://www.quirksmode.org/js/cookies.html
+  function createCookie(name, value, timer) {
+    if (timer) {
+      var date = new Date();
+      //date.setTime(date.getTime()+(timer*60*1000)); //minutes
+      date.setTime(date.getTime()+(timer*24*60*60*1000)); //days
+      var expires = '; expires='+date.toGMTString();
+    } else {
+    	var expires = '';
+    }
+    document.cookie = name+'='+value+expires+'; path=/';
+  }
+  
+  function readCookie(name) {
+    var nameEQ = name + '=';
+    var ca = document.cookie.split(';');
+    for (var i = 0;i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
+  
+  function eraseCookie(name) {
+    createCookie(name,'',-1);
+  }
+  
     /* Copy to clipboard https://stackoverflow.com/a/30810322/7598333*/
   function copyToClipboard(text, elem) {
     var msg, elm = elem || document.body; /* parent element for textarea */
@@ -59,10 +87,49 @@ function mydb_custom() {
     
   }
   
+  function webDarkMode() {
+    // Dark mode
+    if (el('#thememode .switch') || el('.theme.quickswitcher') || el('.theme-mode .switch') || el('.theme.switchmode') || el('#quickswitcher')) {
+      // theme enduser.id|themesia.com || theme eastheme.com || komikcast.com
+      localStorage.setItem('thememode', 'darkmode');
+      localStorage.setItem('theme-mode', 'dark');
+      document.body.classList.add('darkmode', 'dark');
+      document.body.classList.remove('lightmode');
+    } else if (document.body.classList.contains('text-ui-dark')) {
+      // theme mangabooth.com (madara)
+      document.body.classList.add('text-ui-light');
+      document.body.classList.remove('text-ui-dark');
+    } else if (wh.indexOf('manhuaid.com') != -1) {
+      localStorage.setItem('theme', 'dark');
+      document.body.setAttribute('id', 'darkbody');
+      el('.isdark').setAttribute('id', 'darkmode');
+      el('nav').classList.add('bg-dark');
+      el('nav').classList.remove('bg-success', 'fixed-top');
+    } else if (document.body.classList.contains('emissionhex')) {
+      localStorage.setItem('mode', 'darkmode');
+      el('#modeSwitch').classList.add('dark-mode');
+    } else if (wh.indexOf('mangadex') != -1) {
+      var mgdx_css = document.createElement('link');
+      mgdx_css.href = '//mangadex.org/scripts/css/Dark-Slate.css';
+      mgdx_css.setAttribute('rel', 'stylesheet');
+      el('head').appendChild(mgdx_css);
+    } else if (wh.indexOf('mangayu') != -1) {
+      localStorage.setItem('theme', 'dark');
+    } else if (document.body.classList.contains('pemudanolep')) {
+      localStorage.setItem('dark', 'true');
+      document.documentElement.classList.add('dark');
+    } else if (document.body.classList.contains('yuukithemes')) {
+      createCookie('dark', 'yes', '7');
+      document.documentElement.classList.add('dark');
+    }
+  }
+  
   
   var wl = window.location;
   var wh = wl.hostname;
   var wp = wl.pathname;
+  
+  webDarkMode();
   if (wh.indexOf('mangaupdates') != -1 && wl.href.indexOf('/series.html?id=') != -1) copyMU();
 }
 
